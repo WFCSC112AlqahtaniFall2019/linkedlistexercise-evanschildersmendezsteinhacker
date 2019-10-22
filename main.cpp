f556f33f63cd6faba173eaed3f8373365f383264
evanhr18 <evanhr18@wfu.edu>
2019-10-22 15:15:19 -0400

    tasks 1-3

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@ using namespace std;
 
 int main() {
     int x;
+    int minimum;
+    int currentNum;
+    Node* previous;
+    Node* doublePre;
     Node* head= new Node();
     Node* current= head;
     cout<<"enter a list of integers and -1 to stop"<<endl;
@@ -30,11 +34,82 @@ int main() {
 
     //1) find the node that has the minimum value
 
+    current = head;
+    minimum = current->getValue();
+
+    while (current != nullptr) {
+
+        currentNum = current->getValue();
+
+        if (currentNum < minimum) {
+            minimum = currentNum;
+        }
+
+        current = current->getNext();
+
+    }
+
+
     //2) remove the 4th node from the list
+
+    current = head;
+    current = current->getNext();
+    previous = head;
+
+    for (int i = 0; i < 3; i++) {
+        current=current->getNext();
+        previous = previous->getNext();
+    }
+
+    previous->setNext(current->getNext());
+    current->setNext(nullptr);
+    delete current;
+
+    current=head->getNext();
+    while(current!=nullptr)
+    {
+        current->printNode();
+        current=current->getNext();
+    }
 
     //3) remove the last node
 
+    current = head;
+    current = current->getNext();
+    current = current->getNext();
+    previous = head;
+    previous = previous->getNext();
+    doublePre = head;
+
+    while (current != nullptr) {
+        current = current->getNext();
+        previous = previous->getNext();
+        doublePre = doublePre->getNext();
+    }
+
+    doublePre->setNext(nullptr);
+    delete previous;
+
+    current=head->getNext();
+    while(current!=nullptr)
+    {
+        current->printNode();
+        current=current->getNext();
+    }
+
     //4) remove the first node
+
+    current = head;
+
+    head->setNext(head->getNext());
+    delete current;
+
+    current=head->getNext();
+    while(current!=nullptr)
+    {
+        current->printNode();
+        current=current->getNext();
+    }
 
     //6) delete all nodes
     return 0;
